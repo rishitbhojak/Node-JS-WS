cac05e71360c7cda360d1225d5e1d8a56ddeff8d
Rishit Bhojak <rajabhojak@gmail.com>
2021-10-16 10:38:37 +0530

    sorting

--- func.js
+++ func.js
@@ -1,3 +1,4 @@
 const arr = ["5", "2", "3"];
-const sorted = arr.sort((a, b) => a - b);
+const asorted = arr.sort((a, b) => a - b);
+const dsorted = arr.sort((a, b) => b - a);
 console.log("Sorted", arr);
